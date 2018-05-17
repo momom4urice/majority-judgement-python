a559e558cecef0659e30fb4d073633ed2682df8e
Vincent Leroy <maelkoth2@hotmail.fr>
2018-05-17 12:10:51 +0200

    Added CLI with click

--- process.py
+++ process.py
@@ -4,6 +4,7 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import click
 
 from collections import OrderedDict
 from bidict import bidict
@@ -14,7 +15,7 @@ from matplotlib import cm
 
 class Votes(object):
     def __init__(self, name, tally):
-        assert(type(tally) is list)
+        assert (type(tally) is list)
         self.name = name
         self.data = tally
         self.n_votes = sum(self.data)
@@ -37,7 +38,7 @@ class Votes(object):
         return "\n".join(hist)
 
     def __str__(self):
-        return  ("[" + self.name + "] ").ljust(66, "-") + "\n" + self.histogram()
+        return ("[" + self.name + "] ").ljust(66, "-") + "\n" + self.histogram()
 
     def __repr__(self):
         return "Votes(\"{}\", n_bins={}, n_votes={})".format(self.name, self.n_bins, self.n_votes)
@@ -74,6 +75,7 @@ def plot_mj(output_filename, mention_map, votes_dict_mention, candidate_map, can
             raise ValueError("candidate_order list must have the same length as candidate_map")
 
     # Prepare data and plot
+    n_votes = sum([votes_mention[0] for votes_mention in votes_dict_mention.values()])
     ind = range(len(candidate_map.keys()))
     cumulated = np.zeros(len(ind), dtype=int)
 
@@ -83,17 +85,17 @@ def plot_mj(output_filename, mention_map, votes_dict_mention, candidate_map, can
 
     # Build stacked bar plot
     for mention_id in sorted(mention_map.keys()):
-        ## Rearrange candidate order if necessary
+        # -- Rearrange candidate order if necessary
         votes = np.array([votes_dict_mention[mention_id][candidate_id]
                           for candidate_id in candidate_order],
                          dtype=int)
         plot_list.append(plt.bar(ind, votes, bottom=cumulated,
-                                 color=cm.RdYlGn(float(mention_id)/len(mention_map))))
+                                 color=cm.RdYlGn(float(mention_id) / len(mention_map))))
         lgd_labels.append(mention_map[mention_id])
         cumulated += votes
 
     # Add median line
-    plt.plot((ind[0]-1, ind[-1]+1), (n_votes/2., n_votes/2.))
+    plt.plot((ind[0] - 1, ind[-1] + 1), (n_votes / 2., n_votes / 2.))
 
     # Set plot details
     plt.title("RTM Name Vote Analysis")
@@ -101,23 +103,26 @@ def plot_mj(output_filename, mention_map, votes_dict_mention, candidate_map, can
     plt.xticks(ind, [candidate_map[candidate_id]
                      for candidate_id in candidate_order],
                rotation="60")
-    plt.xlim((ind[0]-0.5, ind[-1]+0.5))
+    plt.xlim((ind[0] - 0.5, ind[-1] + 0.5))
     plt.legend([p[0] for p in plot_list[::-1]], lgd_labels[::-1],
                bbox_to_anchor=(1.01, 1.05))
 
     plt.tight_layout()
+    print("Saving plot to {}".format(output_filename))
     plt.savefig(output_filename)
     plt.close()
 
 
-if __name__ == "__main__":
+@click.command(help="Process a CSV file for majority judgement voting.")
+@click.argument("filename")
+def cli(filename):
     # Load CSV data
-    with open('RTM Name.csv', 'rb') as csvfile:
+    with open(filename, 'rb') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
         csv_lines = [line for line in csv_reader]
 
     # Prepare data structures
-    ## Map: mention_name <=> mention_id
+    # -- Map: mention_name <=> mention_id
     mention_map = bidict({0: "To Reject",
                           1: "Poor",
                           2: "Acceptable",
@@ -125,27 +130,30 @@ if __name__ == "__main__":
                           4: "Very Good",
                           5: "Excellent"})
 
+    print("Variable: mention_map ".ljust(66, "-"))
     pprint(dict(mention_map))
 
-    ## Map: candidate_name <=> candidate_id
+    # -- Map: candidate_name <=> candidate_id
     candidate_map = bidict()
     for candidate_id, idx in zip(csv_lines[0][1:], range(len(csv_lines[0][1:]))):
         candidate_map[idx] = candidate_id
+    print("Variable: candidate_map ".ljust(66, "-"))
     pprint(dict(candidate_map))
 
-    ## Dict: candidate_id => per-mention breakdown
+    # -- Dict: candidate_id => per-mention breakdown
     init_list = [0 for i in range(len(mention_map))]
     votes_dict_candidate = OrderedDict([(candidate_id, init_list[:])
                                         for candidate_id in range(len(candidate_map))])
     # pprint(dict(votes_dict_candidate))
 
-    ## Dict: mention_id => per-candidate breakdown
+    # -- Dict: mention_id => per-candidate breakdown
     init_list = [0 for i in range(len(candidate_map))]
     votes_dict_mention = OrderedDict([(mention_id, init_list[:])
                                       for mention_id in range(len(mention_map))])
     # pprint(dict(votes_dict_mention))
 
     # Process data
+    print("Vote processing ".ljust(66, "-"))
     line_id = 0
     for line in csv_lines[1:]:
         line_id += 1
@@ -154,7 +162,7 @@ if __name__ == "__main__":
         for candidate_id in range(len(line) - 1):
             candidate_name = candidate_map[candidate_id]
 
-            mention_name = line[candidate_id+1]
+            mention_name = line[candidate_id + 1]
             try:
                 mention_id = mention_map.inv[mention_name]
                 print("    {}: {}".format(candidate_name, mention_name))
@@ -165,8 +173,9 @@ if __name__ == "__main__":
 
             votes_dict_candidate[candidate_id][mention_id] += 1
             votes_dict_mention[mention_id][candidate_id] += 1
-
+    print("Variable: votes_dict_candidate ".ljust(66, "-"))
     pprint(dict(votes_dict_candidate))
+    print("Variable: votes_dict_mention ".ljust(66, "-"))
     pprint(dict(votes_dict_mention))
 
     # Cast results to Votes
@@ -177,14 +186,21 @@ if __name__ == "__main__":
     votes_list = sorted(votes_list, key=lambda votes: votes.mj, reverse=True)
     for votes in votes_list:
         print(votes)
-    n_votes = votes_list[0].n_votes
+
+    # Make plots
+    sns.set()
+    outfilename_prefix = filename.replace(".csv", "")
 
     # Plot in alphabetical order
-    sns.set()
-    plot_mj("fig/results_alpha.png", mention_map, votes_dict_mention, candidate_map)
+    plot_mj("{}_alpha.png".format(outfilename_prefix),
+            mention_map, votes_dict_mention, candidate_map)
 
     # Plot in MJ order
     candidate_order_mj = [candidate_map.inv[votes.name]
                           for votes in votes_list]
-    plot_mj("fig/results_mj.png", mention_map, votes_dict_mention,
-            candidate_map, candidate_order=candidate_order_mj)
+    plot_mj("{}_mj.png".format(outfilename_prefix),
+            mention_map, votes_dict_mention, candidate_map, candidate_order=candidate_order_mj)
+
+
+if __name__ == "__main__":
+    cli()
